1b5406dbdf4a09e4d3e39bbce333d733e467c2e2
Amit Biswas <biswas1992@hotmail.com>
2019-07-07 03:07:42 -0400

    first commit

---
+++ nativescannerapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nativescannerapp
+//
+//  Created by Maze Geek on 7/7/19.
+//  Copyright © 2019 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
